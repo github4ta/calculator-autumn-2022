f74bf9c28d73d7950e59d2584f19ccc1cc77528b
aleksei <6001mx7@gmail.com>
2022-10-27 20:07:33 +0300

    add generateName method

--- src/by/itacademy/company/Users.java
+++ src/by/itacademy/company/Users.java
@@ -4,6 +4,7 @@ import by.itacademy.user.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Users {
     public List<User> getUsersList(int amount) {
@@ -16,5 +17,15 @@ public class Users {
 
     private int generateNumber(){
     return (int) (Math.random()*99) + 1;
+    }
+
+    private String generateName(){
+        String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String result = "";
+        for (int i = 0; i < 10; i++) {
+            result += abc.charAt((int) (Math.random() * abc.length()));
+        }
+        result = result.substring(0, 1).toUpperCase() + result.substring(1).toLowerCase();
+        return result;
     }
 }

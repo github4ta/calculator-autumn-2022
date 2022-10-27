5ae6ec179ce683bc8c45f6cb9d8300ddadd02bd6
Aliaksandr Viartseika <aliaksandr_viartseika@leverx.com>
2022-10-27 18:31:16 +0200

    add class Users

---
+++ src/by/itacademy/company/Users.java
@@ -0,0 +1,17 @@
+package by.itacademy.company;
+
+import by.itacademy.user.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Users {
+    public List<User> getUsersList(int amount) {
+        List<User> list = new ArrayList<>();
+        User user = new User(13, "Emma");
+        for (int i = 0; i < amount; i++) {
+            list.add(user);
+        }
+        return list;
+    }
+}

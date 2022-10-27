ab12a2cd21cef1c0bb05e49c46d2f67be63c851d
Nataliya Kleschenko <nataliya.kleschenko@gmail.com>
2022-10-27 19:51:29 +0300

    refactor getUsersList method

--- src/by/itacademy/company/Users.java
+++ src/by/itacademy/company/Users.java
@@ -8,14 +8,13 @@ import java.util.List;
 public class Users {
     public List<User> getUsersList(int amount) {
         List<User> list = new ArrayList<>();
-        User user = new User(13, "Emma");
         for (int i = 0; i < amount; i++) {
-            list.add(user);
+            list.add(new User(generateNumber(),"Emma"));
         }
         return list;
     }
 
     private int generateNumber(){
-    return (int) Math.random()*99 + 1;
+    return (int) (Math.random()*99) + 1;
     }
 }

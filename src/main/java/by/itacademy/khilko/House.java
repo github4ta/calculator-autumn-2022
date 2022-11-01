851821b8a072b6acefb8dd2d0f94ec6e711b38a3
aleksei <6001mx7@gmail.com>
2022-11-01 19:31:33 +0300

    New class House

---
+++ src/main/java/by/itacademy/khilko/House.java
@@ -0,0 +1,114 @@
+package by.itacademy.khilko;
+/*
+House: id, Номер квартиры, Площадь, Этаж, Количество комнат, Улица, Тип
+здания, Срок эксплуатации.
+КЛАССЫ И МЕТОДЫ
+113
+Создать массив объектов. Вывести:
+a) список квартир, имеющих заданное число комнат;
+b) список квартир, имеющих заданное число комнат и расположенных на
+этаже, который находится в заданном промежутке;
+c) список квартир, имеющих площадь, превосходящую заданную.
+
+id, Apartment number, Area, Floor, Number of rooms, Street,
+Building type, Service life.
+ */
+public class House {
+    private int id;
+    private int apartmentNumber;
+    private int area;
+    private int floor;
+    private int numberOfRooms;
+    private String street;
+    private String buildingType;
+    private int serviceLife;
+
+    public House(int id, int apartmentNumber, int area, int floor, int numberOfRooms, String street, String buildingType, int serviceLife) {
+        this.id = id;
+        this.apartmentNumber = apartmentNumber;
+        this.area = area;
+        this.floor = floor;
+        this.numberOfRooms = numberOfRooms;
+        this.street = street;
+        this.buildingType = buildingType;
+        this.serviceLife = serviceLife;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getApartmentNumber() {
+        return apartmentNumber;
+    }
+
+    public void setApartmentNumber(int apartmentNumber) {
+        this.apartmentNumber = apartmentNumber;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public void setNumberOfRooms(int numberOfRooms) {
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getBuildingType() {
+        return buildingType;
+    }
+
+    public void setBuildingType(String buildingType) {
+        this.buildingType = buildingType;
+    }
+
+    public int getServiceLife() {
+        return serviceLife;
+    }
+
+    public void setServiceLife(int serviceLife) {
+        this.serviceLife = serviceLife;
+    }
+
+    @Override
+    public String toString() {
+        return "House{" +
+                "id=" + id +
+                ", apartmentNumber=" + apartmentNumber +
+                ", area=" + area +
+                ", floor=" + floor +
+                ", numberOfRooms=" + numberOfRooms +
+                ", street='" + street + '\'' +
+                ", buildingType='" + buildingType + '\'' +
+                ", serviceLife=" + serviceLife +
+                '}';
+    }
+}

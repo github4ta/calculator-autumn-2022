1f98aea3f037da6af9f5d1228e520c115730ec9d
Aliaksandr Viartseika <aliaksandr_viartseika@leverx.com>
2022-11-01 19:06:14 +0100

    add add method

--- src/main/java/by/itacademy/calculator/Calculator.java
+++ src/main/java/by/itacademy/calculator/Calculator.java
@@ -1,11 +1,15 @@
 package by.itacademy.calculator;
 
-public class Calculator{
-    public float subtract(float a, float b){
+public class Calculator {
+    public float subtract(float a, float b) {
         return a + b;
     }
 
     public float divide(float c, float d) {
         return c / d;
+    }
+
+    public float add(float a, float b) {
+        return a * b;
     }
 }

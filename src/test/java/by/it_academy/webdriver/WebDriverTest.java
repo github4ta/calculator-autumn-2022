b1af6a32e05aaf438280f2f65facb6010a7b3201
Viktor Marchuk <viktor.m77.77@mail.ru>
2022-11-03 21:01:16 +0300

    add test class2

--- src/test/java/by/it_academy/webdriver/WebDriverTest.java
+++ src/test/java/by/it_academy/webdriver/WebDriverTest.java
@@ -1,5 +1,16 @@
 package by.it_academy.webdriver;
 
-public class WebDriverTest{
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class WebDriverTest{
+    @Test
+    public void testWebDriver(){
+        // 1. создать экземпляр вэб драйвера
+        String url = "https://www.onliner.by/";
+        ChromeDriver driver = new ChromeDriver();
+        // 2. открыть вэб страницу
+        driver.get(url);
+
+    }
 }

da6bc7c97692cc81b0c112dce606729967cb7f50
hanna_klimasheuskaya <hanna.klimasheuskaya@gmail.com>
2022-11-03 19:07:15 +0300

    add class Abiturients

---
+++ src/main/java/by/itacademy/university/Abiturients.java
@@ -0,0 +1,15 @@
+package by.itacademy.university;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Abiturients {
+    public ArrayList<Abiturient> generateList() {
+        Map<String, Integer> subjectAndMark = new HashMap<>();
+        subjectAndMark.put("Math", 8);
+        ArrayList<Abiturient> abiturients = new ArrayList<>();
+        abiturients.add(new Abiturient("Mike", subjectAndMark);
+        return abiturients;
+    }
+}

eaca10103d721ed817ae8b6cc87eb0231400da8f
hanna_klimasheuskaya <hanna.klimasheuskaya@gmail.com>
2022-11-03 19:15:39 +0300

    add class Main

---
+++ src/main/java/by/itacademy/university/Main.java
@@ -0,0 +1,11 @@
+package by.itacademy.university;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Abiturients abiturients = new Abiturients();
+        ArrayList<Abiturient> listOfAbiturientsWithSubjectsAndMarks = abiturients.generateList();
+        System.out.println(listOfAbiturientsWithSubjectsAndMarks);
+    }
+}

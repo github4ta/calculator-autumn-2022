a4e0518787fab2d689aef1e3743ac953117b6d35
Viktor Marchuk <viktor.m77.77@mail.ru>
2022-11-07 17:51:08 +0300

    commit task07

--- src/main/java/by/itacademy/marchuk_viktor/main/Main.java
+++ src/main/java/by/itacademy/marchuk_viktor/main/Main.java
@@ -3,7 +3,6 @@ package by.itacademy.marchuk_viktor.main;
 import by.itacademy.marchuk_viktor.entity.*;
 import by.itacademy.marchuk_viktor.service.CountingCalories;
 import by.itacademy.marchuk_viktor.service.SearchVegetable;
-import by.itacademy.marchuk_viktor.service.Sorting;
 
 import java.util.ArrayList;
 
@@ -17,29 +16,27 @@ public class Main{
     public static void main(String[] args){
         CountingCalories calories = new CountingCalories();
         SearchVegetable vegetable = new SearchVegetable();
-        Sorting sorting=new Sorting();
 
         ArrayList <Salad> autumnSalads = new ArrayList <>();
         Salad salad = new Salad("Autumn salad", new Cabbage(3.5, 2.4, 4.5, 3.1),
                 new Carrot(3.4, 2.3, 5.7, 3.5),
                 new Cucumber(5.4, 3.5, 2.3, 6.0));
         autumnSalads.add(salad);
-        calories.countAutumnSalad(autumnSalads);
-        vegetable.findVegetableAutumnSalad(2, 4, autumnSalads);
+        System.out.println(calories.countAutumnSalad(autumnSalads));
+        System.out.println(vegetable.findVegetableAutumnSalad(2, 4, autumnSalads));
 
         ArrayList <Salad> springSalad = new ArrayList <>();
         springSalad.add(new Salad("Spring salad", new Tomato(2.3, 4.3, 5.6, 6.4),
                 new Cucumber(4.5, 3.2, 5.6, 4.5)));
-        calories.countSpringSalad(springSalad);
-        vegetable.findVegetableSpringSalad(3, 6, springSalad);
-        sorting.sortByProteins(springSalad);
+        System.out.println(calories.countSpringSalad(springSalad));
+        System.out.println(vegetable.findVegetableSpringSalad(3, 6, springSalad));
 
         ArrayList <Salad> summerSalad = new ArrayList <>();
         summerSalad.add(new Salad("Summer salad", new Pepper(12.4, 1.5, 3.6, 3.5),
                 new Cucumber(2.3, 3.4, 5.2, 6.4),
                 new Tomato(2.4, 3.4, 6.2, 3.5)));
-        calories.countSummerSalad(summerSalad);
-        vegetable.findVegetableSummerSalad(23, 56, summerSalad);
+        System.out.println(calories.countSummerSalad(summerSalad));
+        System.out.println(vegetable.findVegetableSummerSalad(23, 56, summerSalad));
 
     }
 }
--- src/main/java/by/itacademy/marchuk_viktor/service/CountingCalories.java
+++ src/main/java/by/itacademy/marchuk_viktor/service/CountingCalories.java
@@ -5,30 +5,30 @@ import by.itacademy.marchuk_viktor.entity.Salad;
 import java.util.ArrayList;
 
 public class CountingCalories{
-    public void countAutumnSalad(ArrayList <Salad> salads){
+    public double countAutumnSalad(ArrayList <Salad> salads){
         System.out.println("All calories in autumn salad");
         double sum = 0;
         for(Salad salad: salads){
             sum = salad.getCabbage().getCalories() + salad.getCarrot().getCalories() + salad.getCucumber().getCalories();
         }
-        System.out.println(sum);
+        return sum;
     }
 
-    public void countSpringSalad(ArrayList <Salad> salads){
+    public double countSpringSalad(ArrayList <Salad> salads){
         System.out.println("All calories in spring salad");
         double sum = 0;
         for(Salad salad: salads){
             sum = salad.getTomato().getCalories() + salad.getCucumber().getCalories();
         }
-        System.out.println(sum);
+        return sum;
     }
 
-    public void countSummerSalad(ArrayList <Salad> salads){
+    public double countSummerSalad(ArrayList <Salad> salads){
         System.out.println("All calories in summer salad");
         double sum = 0;
         for(Salad salad: salads){
             sum = salad.getPepper().getCalories() + salad.getCucumber().getCalories() + salad.getTomato().getCalories();
         }
-        System.out.println(sum);
+        return sum;
     }
 }
